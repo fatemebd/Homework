ea30810cc5635cf5afc7f3dcf203308f5836ed51
fatemebd <fateme.s.e.81@gmail.com>
2024-06-04 23:46:17 +0330

    merge

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -6,27 +6,40 @@ import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Build
-
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.homework.ui.theme.HomeworkTheme
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.io.File
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.File
@@ -35,6 +48,7 @@ import java.util.concurrent.TimeUnit
 class MainActivity : ComponentActivity() {
     private lateinit var connectivityReceiver: ConnectivityReceiver
     private val connectivityStatus = mutableStateOf("unknown")
+    private var logList by mutableStateOf<List<LogEntry>>(emptyList())
 
     companion object {
         const val REQUEST_CODE_POST_NOTIFICATIONS = 1
@@ -49,10 +63,7 @@ class MainActivity : ComponentActivity() {
 
         WorkManager.getInstance(this).enqueue(workRequest)
 
-        readLogFile()
-
-
-        readLogFile()
+        logList = readLogFile()
 
         connectivityReceiver = ConnectivityReceiver { status ->
             connectivityStatus.value = status
@@ -65,10 +76,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             HomeworkTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        status = connectivityStatus.value,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                            .fillMaxSize()
+                    ) {
+                        ConnectivityStatusText(connectivityStatus.value)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        LogList(logs = logList)
+                    }
                 }
             }
         }
@@ -123,7 +140,7 @@ class MainActivity : ComponentActivity() {
         unregisterReceiver(connectivityReceiver)
     }
 
-    private fun readLogFile() {
+    private fun readLogFile(): List<LogEntry> {
         val fileName = "log.json"
         val file = File(filesDir, fileName)
         val gson = Gson()
@@ -135,41 +152,107 @@ class MainActivity : ComponentActivity() {
             mutableListOf()
         }
 
-        logList.forEach { logEntry ->
-            Log.v("tagA", logEntry.toString())
-        }
+        logList.sortByDescending { it.time }
+        return logList
     }
+}
 
-    private fun readLogFile() {
-        val fileName = "log.json"
-        val file = File(filesDir, fileName)
-        val gson = Gson()
-        val logList: MutableList<LogEntry> = if (file.exists()) {
-            val existingLogs = file.readText()
-            val type = object : TypeToken<MutableList<LogEntry>>() {}.type
-            gson.fromJson(existingLogs, type) ?: mutableListOf()
-        } else {
-            mutableListOf()
-        }
+@Composable
+fun ConnectivityStatusText(status: String) {
+    Text(
+        text = "Internet is $status",
+        style = MaterialTheme.typography.titleLarge.copy(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        ),
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(bottom = 8.dp)
+    )
+}
 
-        logList.forEach { logEntry ->
-            Log.v("tagB", logEntry.toString())
+@Composable
+fun LogList(logs: List<LogEntry>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier.padding(16.dp)) {
+        items(logs) { log ->
+            LogCard(logEntry = log)
         }
     }
 }
 
 @Composable
-fun Greeting(status: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Internet is $status",
-        modifier = modifier
-    )
+fun LogCard(logEntry: LogEntry) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Time:",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = logEntry.time,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(2f)
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Status Type:",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = logEntry.statusType,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(2f)
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Status:",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = logEntry.status,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(2f)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LogListPreview() {
     HomeworkTheme {
-        Greeting("unknown")
+        LogList(
+            logs = listOf(
+                LogEntry("2023-06-04 12:00:00", "bluetooth", "enabled"),
+                LogEntry("2023-06-04 12:15:00", "airplane", "disabled")
+            )
+        )
     }
 }
