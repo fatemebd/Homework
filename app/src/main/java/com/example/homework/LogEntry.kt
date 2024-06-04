febef4dcf11eee02c57303c77cdc831d32c7462b
fatemebd <fateme.s.e.81@gmail.com>
2024-06-04 23:30:16 +0330

    add code_2

---
+++ app/src/main/java/com/example/homework/LogEntry.kt
@@ -0,0 +1,3 @@
+package com.example.homework
+
+data class LogEntry(val time: String, val statusType: String, val status: String)
--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.homework
 
+
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,12 +13,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.homework.ui.theme.HomeworkTheme
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val workRequest = PeriodicWorkRequestBuilder<StatusWorker>(15, TimeUnit.MINUTES)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(workRequest)
+
+        readLogFile()
+
         setContent {
             HomeworkTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -26,6 +42,23 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    private fun readLogFile() {
+        val fileName = "log.json"
+        val file = File(filesDir, fileName)
+        val gson = Gson()
+        val logList: MutableList<LogEntry> = if (file.exists()) {
+            val existingLogs = file.readText()
+            val type = object : TypeToken<MutableList<LogEntry>>() {}.type
+            gson.fromJson(existingLogs, type) ?: mutableListOf()
+        } else {
+            mutableListOf()
+        }
+
+        logList.forEach { logEntry ->
+            Log.v("tagB", logEntry.toString())
         }
     }
 }
